920b7c544fd6b40d2053b4e7de42892aacd22097
saradha <saradhashyam0401@gmail.com>
2024-04-30 15:22:09 +0530

    make darker

--- src/TextForm.js
+++ src/TextForm.js
@@ -2,17 +2,51 @@ import React,{useState} from 'react'
 // import PropTypes from 'prop-types'
 
 
-
+// This is the area where we define the function for the buttons
 export default function TextForm(props) {
+    const hadleUpClick =(event)=>{
+        console.log("handleUp  been clicked");
+        // setText("I've been changed by settext");
+        let t = text.toUpperCase();
+        setText(t);
+    }
+    const handleOnChange =(event)=>{
+        console.log("handleOnChange  been clicked");
+        setText(event.target.value);
+    }
+    const hadlDownClick =(event)=>{
+        console.log("handleDown been clicked");
+        let t = text.toLowerCase();
+        setText(t);
+
+    }
+
     const [text,setText] = useState("Enter ur comment here");
+    // 'text' cannot be assigned a value directly , instead we have to use 'setText' for it
+    // text = " This is the new text"; //This is the wrong way
+    // setText("This is the new Text"); // This is the correct way of assigning a value
+
+
 return(
     <>
-    <div class="form-floating">
-        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-        <label htmlForfor="floatingTextarea">{text}</label>
+    <div className="container">
+        <h2>Enter your text here </h2>
+        <div className="form-floating">
+            <textarea className="form-control" onChange={handleOnChange}  id="floatingTextarea" value={text}></textarea>
+            <label htmlFor="floatingTextarea">Enter text here</label>
+        </div>
+        <br/>
+        <button type="button" className="btn btn-info" onClick={hadleUpClick}>Convert to upper case</button>
+        <button type="button" className="btn btn-success" onClick={hadlDownClick}>Convert to lower case</button>
     </div>
-    <br/>
-    <button type="button" class="btn btn-info">Convert to upper case</button>
+    
+    <div className="container">
+        <h4> Words and Character typed</h4>
+        <p> No of words : - {text.split(" ").length}</p>
+        <p>No of Characters: - {text.length}</p>
+
+
+    </div>
     </>
 )
 
