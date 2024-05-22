231b92f67f3d33dd7f70e8f3b45e25076077dce8
saradha <saradhashyam0401@gmail.com>
2024-05-22 17:11:59 +0530

    use state for adding light and dark mode manually

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import TextForm from './TextForm';
 import About from './components/About';
 import Navbar from './components/Navbar';
+// import React, { useState } from 'react';
+
 
 function App() {
+  const [mode,setMode] = useState("dark");
   return (
     <>
-      <Navbar  title = "Cutie CAT App" about = "Cuteness"/> 
+      <Navbar  title = "Cutie CAT App" about = "Cuteness" mode = {mode}/> 
       <TextForm placeHold= "enter something here..."/>
       <About/>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     // <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
